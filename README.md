8861f70b3060d40893fdd548b14609dce3e1d221
Thiago Marinho <tgmarinho@gmail.com>
2019-09-24 15:17:23 -0400

    CRA ReactJS - Aula 10 - Navegação de rotas

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-## Aula 09 - Utilizando LocalStorage
 
-Vamos salvar os repositórios no localstorage, que é um banco de dados embutido no navegador que armazena chave valor em Strings.
-
+## Aula 10 - Navegação de rotas
 
 
-Fim, código fonte: [https://github.com/tgmarinho/front-react/tree/aula09-utilizando-localstorage](https://github.com/tgmarinho/front-react/tree/aula09-utilizando-localstorage)
+
+
+Fim, código fonte: [https://github.com/tgmarinho/front-react/tree/aula10-navegacao-de-rotas](https://github.com/tgmarinho/front-react/tree/aula10-navegacao-de-rotas)
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import { Container, Form, SubmitButton, List } from './styles';
 
 import api from '../../services/api';
@@ -80,7 +81,9 @@ class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href="">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
