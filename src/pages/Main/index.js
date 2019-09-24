00c5d372b83467d66ddb0822453fe26c53dff1d6
Thiago Marinho <tgmarinho@gmail.com>
2019-09-24 11:51:40 -0400

    CRA ReactJS - Aula 06 - Estilizando a pagina Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-const Main = () => <Title>Main</Title>;
+import { Container, Form, SubmitButton } from './styles';
+
+const Main = () => (
+  <Container>
+    <h1>
+      <FaGithubAlt />
+      Repositórios
+    </h1>
+
+    <Form onSubmit={() => {}}>
+      <input type="text" placeholder="Adicionar repositório" />
+
+      <SubmitButton>
+        <FaPlus color="#FFF" size={14} />
+      </SubmitButton>
+    </Form>
+  </Container>
+);
 
 export default Main;
